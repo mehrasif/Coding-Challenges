4cdb3a46e1f3a84d82ab49e300745ed27328356a
mehrasif <mehrina.asif@gmail.com>
2022-05-10 17:18:56 -0400

    second update for today

--- 05-10-2022.js
+++ 05-10-2022.js
@@ -3,3 +3,9 @@
 function smash (words) {
     return words.join(' ')
  };
+
+ //Code as fast as you can! You need to double the integer and return it.
+
+ function doubleInteger(i) {
+    return (i*2);
+  }
